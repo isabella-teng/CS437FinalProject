815008eee1af48ee32cb359f7122bd0da2d832a8
Isabella <isabella.teng@yale.edu>
2018-11-03 18:47:55 -0400

    more boilerplate

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# def hello_world():
+#     return 'Hello, World!'
+
+def index():
+    return render_template('index.html')
+
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
