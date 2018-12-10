427361107c9c88e5344363acbb4fc0811570be94
Isabella <isabella.teng@yale.edu>
2018-12-10 18:27:22 -0500

    send array back to flask

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 from flask import g
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
 
@@ -28,9 +28,15 @@ def index():
         table_data = query_db('SELECT * from artist')
         return render_template('index.html', data=table_data) #TODO: we only want hottest 100
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/receivedSortings', methods = ['POST'])
+def fetchPlaylist():
+    if request.method == 'POST':
+        data = request.json
+
+        for artist in data:
+            print("query the songs for artist id: " + artist)
+
+    return render_template('hello.html') #TODO replace with html file for the returned playlist page
 
 
 if __name__ == '__main__':
